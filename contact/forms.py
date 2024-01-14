54d8cfb18c2cb12ad0f0ff7b95ad5b5b8763fcd5
JeffersonEstevo <jefferson.estevo@gmail.com>
2024-01-14 17:59:34 -0300

    Configurando os campos e widgets do formulário

--- contact/forms.py
+++ contact/forms.py
@@ -3,14 +3,55 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
+    # mudar campos do form declarado em models.py
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Escreva aqui',       
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Texto de ajuda para seu usuário',
+    )    
+
+    # Podemos criar novos campos no form
+    # qualquer = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             'class': 'classe-a classe-b',
+    #             'placeholder': 'Escreva aqui',       
+    #         }
+    #     ),
+    #     help_text='Texto de ajuda para seu usuário',
+    # ) 
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # outra maneira de atualizar um cmapo já existente no form
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'classe-a classe-b',
+        #     'placeholder': 'Aqui veio do init',             
+        # })
+
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
         )
+        # outra maneira de atualizar um cmapo já existente no form
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b',
+        #             'placeholder': 'Escreva aqui',       
+        #         }
+        #     )
+        # }
 
     def clean(self):
-        cleaned_data = self.cleaned_data
+        #cleaned_data = self.cleaned_data
         #print(cleaned_data)
 
         self.add_error(
