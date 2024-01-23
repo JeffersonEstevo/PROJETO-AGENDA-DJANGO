8c3280a36f9cbf4cefadd174b20f485b4d2a1509
JeffersonEstevo <jefferson.estevo@gmail.com>
2024-01-22 21:53:48 -0300

    Criando a relação entre contact e owner (request.user)

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -43,7 +43,6 @@ def create(request):
 @login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
         Contact, pk=contact_id, show=True, owner=request.user
     )
 
@@ -81,7 +80,6 @@ def update(request, contact_id):
 @login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
         Contact, pk=contact_id, show=True, owner=request.use
     )
     confirmation = request.POST.get('confirmation', 'no')
